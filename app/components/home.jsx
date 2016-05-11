036baaa425daef323fe9afe7c4bcc67b1242fbc8
Craig Bengtson <cbengtson85@gmail.com>
2016-05-11 16:04:47 -0400

    remove test module

--- app/components/home.jsx
+++ app/components/home.jsx
@@ -3,7 +3,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-let text = require('app/components/test');
-
-
 ReactDOM.render(<div>Hello world from React</div>, document.getElementById('app'));
