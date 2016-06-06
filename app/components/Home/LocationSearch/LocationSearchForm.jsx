68fac65c372fb8e479d56de6fca9e72617303537
Craig Bengtson <cbengtson85@gmail.com>
2016-06-05 23:38:01 -0500

    remove dom manipulation

--- app/components/Home/LocationSearch/LocationSearchForm.jsx
+++ app/components/Home/LocationSearch/LocationSearchForm.jsx
@@ -10,7 +10,6 @@ class LocationSearchForm extends React.Component  {
     handleClear(e) {
         e.preventDefault();
         const {dispatch} = this.props;
-        $('#search-input').val('');
         dispatch(returnNoResults(''));
     }
 
@@ -49,9 +48,9 @@ class LocationSearchForm extends React.Component  {
         const {dispatch, searchTermLength} = this.props;
         return (
             <form action="#" className="find-location">
-                <input id="search-input" autoComplete="off" onKeyUp={e => this.handleSearchKeyUp(e)}
+                <input autoComplete="off" onKeyUp={e => this.handleSearchKeyUp(e)}
                     type="text" placeholder="Find your location..." maxLength="100"
-                     value={this.props.searchVal} onChange={e => this.handleChange(e)}/>
+                    value={this.props.searchVal} onChange={e => this.handleChange(e)}/>
                 {this.props.isLoading ? <span className="search-loader"><AjaxSpinner height="40" /></span> : ''}
                 {this.props.searchTermLength > 0 && !this.props.isLoading ?
                     <a href="#" className="clear-field" onClick={e => this.handleClear(e)}><ClearIcon height="20" /></a> : ''}
