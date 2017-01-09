885c4b03d80b6cddaa2741ff011ee58aab6ddd7a
Craig Bengtson <cbengtson85@gmail.com>
2017-01-09 17:09:46 -0500

    fix typo

--- app/components/Home/Weather/index.js
+++ app/components/Home/Weather/index.js
@@ -4,5 +4,5 @@ export {default as WeatherContainer} from 'app/components/Home/Weather/WeatherCo
 export {default as CurrentWeather} from 'app/components/Home/Weather/CurrentWeather.jsx';
 export {default as WeatherForecastItem} from 'app/components/Home/Weather/WeatherForecastItem.jsx';
 export {default as LoadingIndicator} from 'app/components/Home/Weather/LoadingIndicator.jsx';
-export {default as ExtendedForecast} from 'app/components/Home/Weather/ExtendedForeCast.jsx';
+export {default as ExtendedForecast} from 'app/components/Home/Weather/ExtendedForecast.jsx';
 export {default as HourlyForecast} from 'app/components/Home/Weather/HourlyForecast.jsx';
