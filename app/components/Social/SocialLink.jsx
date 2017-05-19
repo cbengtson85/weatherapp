c861e18f0400cc32de9353f963ad6b8c576c8326
Craig Bengtson <cbengtson85@gmail.com>
2017-05-19 09:49:40 -0400

    update dependencies

--- app/components/Social/SocialLink.jsx
+++ app/components/Social/SocialLink.jsx
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 
 const SocialLink = ({site}) => {
     return (
-        <a href={site.url} target="_blank"><i className={'fa fa-' + site.name}></i></a>
+        <a href={site.url} target="_blank" rel="noopener noreferrer"><i className={'fa fa-' + site.name}></i></a>
     )
 };
 
